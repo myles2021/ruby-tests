3715d1d42f5c2585e9a868295c06527d3c29caf3
myles2021 <mylesegan95@gmail.com>
2022-02-10 14:47:15 +0000

    new card game online

--- poophead_card_game/poophead.rb
+++ poophead_card_game/poophead.rb
@@ -6,56 +6,63 @@ def players_go
   turn = true
 end
 
-def in_play_pile
-end
+in_play_pile = []
 
 correct_card = false
 
 def card_rules
   if card_picked == "A"
-    if in_play_pile != 7
+    if in_play_pile(0) != 7
       correct_card = true
     end
   elsif card_picked == "K"
-    if in_play_pile != 7 || in_play_pile != "A"
+    if in_play_pile(0) != (7 || "A")
       correct_card = true
     end
   elsif card_picked == "Q"
-    if in_play_pile != 7 || in_play_pile != ("A" || "K")
+    if in_play_pile(0) != (7 || "A" || "K")
       correct_card = true
     end
   elsif card_picked == "J"
-    if in_play_pile != 7 || in_play_pile != ("A" || "K" || "Q")
+    if in_play_pile(0) != (7 || "A" || "K" || "Q")
       correct_card = true
     end
   elsif card_picked == "10"
       correct_card = true
+      in_play_pile(0).delete_all
   elsif card_picked == "9"
-    if in_play_pile != (7 || "A" || "K" || "Q" || "J")
+    if in_play_pile(0) != (7 || "A" || "K" || "Q" || "J")
       correct_card = true
     end
   elsif card_picked == "8"
       correct_card = true
   elsif card_picked == "7"
-    if in_play_pile ("A" || "K" || "Q" || "J" || 9)
+    if in_play_pile(0) != ("A" || "K" || "Q" || "J" || 9)
       correct_card = true
     end
   elsif card_picked == "6"
-    if in_play_pile != ("A" || "K" || "Q" || "J" || 9)
+    if in_play_pile(0) != ("A" || "K" || "Q" || "J" || 9)
       correct_card = true
     end
   elsif card_picked == "5"
-    if in_play_pile != ("A" || "K" || "Q" || "J" || 9 || 6)
+    if in_play_pile(0) != ("A" || "K" || "Q" || "J" || 9 || 6)
       correct_card = true
     end
     players_go
   elsif card_picked == "4"
-    if in_play_pile != ("A" || "K" || "Q" || "J" || 9 || 6 || 5)
+    if in_play_pile(0) != ("A" || "K" || "Q" || "J" || 9 || 6 || 5)
       correct_card = true
     end
   elsif card_picked == "3"
-    if in_play_pile != ("A" || "K" || "Q" || "J" || 9 || 6 || 5 || 3)
+    if in_play_pile(0) != ("A" || "K" || "Q" || "J" || 9 || 6 || 5 || 3)
       correct_card = true
     end
+  elsif card_picked == "Joker"
+    correct_card = true
+    puts "What card will your Joker be impersonating: \n"
+    joker_face = gets.chomp.uppercase
+    card_picked = joker_face
   end
+else
+  correct_card = true
 end
