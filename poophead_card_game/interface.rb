a06c4a9a6cd65481116a3ddcde19a0d93b6100e9
myles2021 <mylesegan95@gmail.com>
2022-02-11 16:47:56 +0000

    intro fix, and player select fix

--- poophead_card_game/interface.rb
+++ poophead_card_game/interface.rb
@@ -9,24 +9,49 @@ players_question = true
 while players_question == true
   puts "How many players are playing? [1] [2] [3] [4]"
   amount_of_players = gets.chomp.to_i
-  if amount_of_players >= 1
-    player1_top_cards = []
-    player1_bottom_cards = []
-  elsif amount_of_players >= 2
-    player2_bottom_cards = []
-    player2_top_cards = []
-  elsif amount_of_players >= 3
-    player3_bottom_cards = []
-    player3_top_cards = []
+  if amount_of_players == 1
+    one_player = [
+      player1_top_cards = [],
+      player1_bottom_cards = []
+    ]
+    players_question = false
+  elsif amount_of_players == 2
+    two_players = [
+      player1_top_cards = [],
+      player1_bottom_cards = [],
+      player2_top_cards = [],
+      player2_bottom_cards = []
+    ]
+    players_question = false
+  elsif amount_of_players == 3
+    three_players = [
+      player1_top_cards = [],
+      player1_bottom_cards = [],
+      player2_top_cards = [],
+      player2_bottom_cards = [],
+      player3_top_cards = [],
+      player3_bottom_cards = []
+    ]
+    players_question = false
   elsif amount_of_players == 4
-    player4_bottom_cards = []
-    player4_top_cards = []
+    four_players = [
+      player1_top_cards = [],
+      player1_bottom_cards = [],
+      player2_top_cards = [],
+      player2_bottom_cards = [],
+      player3_top_cards = [],
+      player3_bottom_cards = [],
+      player4_top_cards = [],
+      player4_bottom_cards = []
+    ]
+    players_question = false
   else
     puts "Please only select 1, 2, 3, or 4 players."
     players_question = true
   end
-  players_question = false
 end
+
+print four_players
 
 # running = true
 
