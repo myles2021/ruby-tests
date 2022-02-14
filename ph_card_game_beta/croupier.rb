cec535b598244149e3aeeeb79a5e6cfa3c325402
myles2021 <mylesegan95@gmail.com>
2022-02-14 11:34:19 +0000

    suits added to cards

--- ph_card_game_beta/croupier.rb
+++ ph_card_game_beta/croupier.rb
@@ -1,14 +1,16 @@
-@card_deck = ["2", "2", "2", "2", "3", "3", "3", "3", "4", "4", "4", "4", "5", "5", "5", "5", "6", "6", "6", "6", "7", "7", "7", "7", "8", "8", "8", "8", "9", "9", "9", "9", "10", "10", "10", "10", "J", "J", "J", "J", "Q", "Q", "Q", "Q", "K", "K", "K", "K", "A", "A", "A", "A", "Joker", "Joker"]
+# @card_deck = ["2", "2", "2", "2", "3", "3", "3", "3", "4", "4", "4", "4", "5", "5", "5", "5", "6", "6", "6", "6", "7", "7", "7", "7", "8", "8", "8", "8", "9", "9", "9", "9", "10", "10", "10", "10", "J", "J", "J", "J", "Q", "Q", "Q", "Q", "K", "K", "K", "K", "A", "A", "A", "A", "Joker", "Joker"]
 
-# @card_deck = []
+@card_deck = []
+suits = %w[S C D H]
+characters = %w[2 3 4 5 6 7 8 9 10 J Q K A]
 
-# suits = %w[S C D H]
+characters.each do |character|
+  suits.each do |suit|
+    @card_deck << "#{suit}#{character}"
+  end
+end
 
-# characters = %w[2 3 4 5 6 7 8 9 10 J Q K A]
-
-# suits.each do |suit|
-#   @card_deck << "#{suit} #{characters[(0..13)]}"
-# end
+@card_deck.push('Joker1', 'Joker2')
 
 @player_one = [
   player1_hand_cards = [],
@@ -33,39 +35,7 @@ def deal_cards
   end
 end
 
-# def no_nil_entry
-#   if @player_one[0..2].include?(nil) && @player_two[0..2].include?(nil)
-#     until @player_one[0..2].size == 9 && @player_two[0..2].size == 9
-#       @player_one[0..2].pop until @player_one[0..2].empty?
-#       @player_two[0..2].pop until @player_two[0..2].empty?
-#       deal_cards until
-#     end
-#   end
-# end
-# def check_p1_cards
-#   @player_one.each do |card_group|
-#     if card_group.include?(nil)
-#       card_group.pop until card_group.empty?
-#       deal_cards
-#     end
-#   end
-# end
-
-# def check_p2_cards
-#   @player_two.each do |card_group|
-#     if card_group.include?(nil)
-#       card_group.pop until card_group.empty?
-#       deal_cards
-#     end
-#   end
-# end
-
-# @player_one[0] << @card_deck.delete_at(rand(0..card_count))
-# @player_one[1] << @card_deck.delete_at(rand(0..card_count))
-# @player_one[2] << @card_deck.delete_at(rand(0..card_count))
-# @player_two[0] << @card_deck.delete_at(rand(0..card_count))
-# @player_two[1] << @card_deck.delete_at(rand(0..card_count))
-# @player_two[2] << @card_deck.delete_at(rand(0..card_count))
+# card_group.pop until card_group.empty?
 
 # print "#{@player_one} \n"
 # print "#{@player_two} \n"
